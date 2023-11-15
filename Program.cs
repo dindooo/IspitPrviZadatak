7182cf55ab51a5b63ee1c4a442c56e773f092e8a
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-15 09:26:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ispitPrvi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Upisite jednu recenicu");
+            string niz = Console.ReadLine();
+            Console.WriteLine(niz.Replace(" ", "_"));
+            Console.ReadKey();
+
+        }
+    }
+}
